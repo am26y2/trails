fedc7c7f8845e1656f51bd660905dda59b357e7e
Unmilon Borthakur <unmilon.7122@gmail.com>
2021-04-03 00:28:17 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,90 @@
+TweenLite.defaultEase = Linear.easeNone;
+
+var cx = 200;
+var cy = 200;
+
+var svgns = "http://www.w3.org/2000/svg";
+var root  = document.querySelector("svg");
+var twoPi = Math.PI * 2;
+var ease  = 0.35;
+var total = 25;
+
+var colors = ["#EA4335"];
+
+colors.forEach(function(color) {
+  
+  var leader = createPoint(color);
+  console.log('leader: ',leader);
+  
+  var i = total;  
+  while (i--) {
+    var alpha = (i + 1) / total;
+    leader = createLine(leader, alpha, color);
+  }
+});
+
+TweenLite.from("line, circle", 1.5, { alpha: 0 });
+
+
+
+function createPoint(fill) {
+  
+  var circle = document.createElementNS(svgns, "circle");
+  root.appendChild(circle);
+  
+  // var radius = random(75, 500);
+  
+  TweenLite.set(circle, {
+    attr: { r: 9.5, fill: fill },
+    x: 25,
+    y: 380
+  });
+
+  TweenMax.to(circle, 8, {
+    x: 700,
+    y: -280
+  })
+  
+  return circle._gsTransform;
+}
+
+
+function createLine(leader, alpha, stroke) {
+  
+  var line = document.createElementNS(svgns, "line");
+  root.appendChild(line);
+  
+  TweenLite.set(line, {
+    alpha: alpha,
+    stroke: stroke,
+    x: 25,
+    y: 380
+  });
+    
+  var pos = line._gsTransform;
+  
+  TweenMax.to(line, 1000, {
+    x: "+=1",
+    y: 50,
+    // repeat: -1,
+    modifiers: {
+      x: function(x) {        
+        x = pos.x + (leader.x - pos.x) * ease;
+        line.setAttribute("x2", leader.x - x);
+        return x;
+      },
+      y: function(y) {        
+        y = pos.y + (leader.y - pos.y) * ease + random(0.5, 6);
+        line.setAttribute("y2", leader.y - y);
+        return y;
+      }
+    }
+  });  
+  
+  return pos;
+}
+
+function random(min, max) {
+  if (max == null) { max = min; min = 0; }
+  return Math.random() * (max - min) + min;
+}
